71ad44e533bfb93f882e0ee968da214d343ffb39
laurennk <laurennk13@gmail.com>
2022-08-02 19:33:49 +0000

    added print_board()

--- Battleship.py
+++ Battleship.py
@@ -1,3 +1,16 @@
+def print_board(board):
+    extra_line = "     |     |     |     |"
+    new_line = "-----|-----|-----|-----|-----"
+    
+    print(extra_line)
+
+    for i in range(5):
+        print(" ", board[i][0], " | ", board[i][1], " | ", board[i][2], " | ", board[i][3], " | ", board[i][4])
+        print(new_line)
+
+    print(extra_line)
+
+
 def place_ship():
     print("placing ship ...")
 
@@ -18,4 +31,6 @@ if __name__ == '__main__':
                [" ", " ", " ", " ", " "],
                [" ", " ", " ", " ", " "]]
 
+    print_board(array_1)
+    
     place_ship(array_1, 15, "R")
--- DotsAndBoxes.py
+++ DotsAndBoxes.py
@@ -1,3 +1,13 @@
+def print_board(board):
+    print("\n")
+    for i in range(5):
+        if i%2 == 0:
+            print (".", board[i][0], ".", board[i][1], ".", board[i][2], ".")
+        else:
+            print (board[i][0], " ", board[i][1], " ", board[i][2], " ", board[i][3])
+    print("\n")
+
+
 def complete_box():
     print("counting completed boxes ...")
 
@@ -15,6 +25,8 @@ if __name__ == '__main__':
                ["_", "_", "_"],
                ["|", "|", " ", " "],
                ["_", " ", " "]]
+
+    print_board(array_1)
 
     complete_box(array_1)
 
